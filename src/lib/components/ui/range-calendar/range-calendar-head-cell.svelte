6b0d6bede75b43038871f291839267cd3affbcd5
Aidan Neel <118433139+aidan-neel@users.noreply.github.com>
2024-02-28 18:47:20 -0600

    Initial commit

---
+++ src/lib/components/ui/range-calendar/range-calendar-head-cell.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { RangeCalendar as RangeCalendarPrimitive } from "bits-ui";
+	import { cn } from "$lib/utils";
+
+	type $$Props = RangeCalendarPrimitive.HeadCellProps;
+
+	let className: $$Props["class"] = undefined;
+	export { className as class };
+</script>
+
+<RangeCalendarPrimitive.HeadCell
+	class={cn("text-muted-foreground rounded-md w-8 font-normal text-[0.8rem]", className)}
+	{...$$restProps}
+>
+	<slot />
+</RangeCalendarPrimitive.HeadCell>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+    import '../global.css'
+</script>
+
+<body class="dark bg-zinc-950">
+    <slot></slot>
+</body>
